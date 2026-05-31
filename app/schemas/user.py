b774668ccedfc6c4f74a04e65c5cff8b78fae706
joaocrespin <joaorato58@gmail.com>
2026-05-31 09:55:33 -0300

    feat: add email format and password length validation on user registration

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
 
 class UserCreate(BaseModel):
     name:str
-    email: str
-    password: str
+    email: EmailStr
+    password: str = Field(min_length=8)
 
 
 class UserResponse(BaseModel):
